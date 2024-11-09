339f6466060e8d073b35bd54945bd930285badbf
Marianna <mariannaniko1092@gmail.com>
2024-11-09 18:22:52 -0500

    Update render() in shapes.js and shapes.test.js files

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 const inquirer = require("inquirer");
 
+inquirer.prompt([
+    {
+        type: "input",
+        message: "Enter three letters:",
+        name: "text",
+        validate: (text) => (text.length == 3 ? true : false),
+    },
+
+    {
+        type: "input",
+        message: "Enter a text color:",
+        name: "textColor"
+    },
+
+    {
+        type: "input",
+        message: "Enter a shape color:",
+        name: "shapeColor"
+    },
+
+    {
+        type: "list",
+        message: "Choose your favorite shape:",
+        choices: ["Triangle", "Square", "Circle"],
+        name: "shape"
+    },
+
+
+]);
