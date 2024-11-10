3ca5767d9905a5c50edcd1642b6b3f1fde4b2b74
Marianna <mariannaniko1092@gmail.com>
2024-11-09 19:30:24 -0500

    Add inquirer prompt method and fs functionalities to index.js file

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const inquirer = require("inquirer");
+const fs = require("fs");
+const Triangle = require("./lib/triangle");
+const Square = require("./lib/square");
+const Circle = require("./lib/circle");
+
 
 inquirer.prompt([
     {
         type: "input",
-        message: "Enter three letters:",
+        message: "Enter three letters or less:",
         name: "text",
-        validate: (text) => (text.length == 3 ? true : false),
+        validate: (text) => (text.length <= 3 ? true : false),
     },
 
     {
@@ -28,4 +33,23 @@ inquirer.prompt([
     },
 
 
-]);
+])
+.then(res => {
+    let renderedContent = "";
+    let fileName = "";
+    if(res.shape === "Triangle") {
+        const triangle = new Triangle(res.text, res.textColor, res.shapeColor);
+        renderedContent = triangle.render();
+        fileName = "./examples/triangle.svg";
+    } else if(res.shape === "Square") {
+        const square = new Square(res.text, res.textColor, res.shapeColor);
+        renderedContent = square.render();
+        fileName = "./examples/square.svg";
+    } else if(res.shape === "Circle") {
+        const circle = new Circle(res.text, res.textColor, res.shapeColor);
+        renderedContent = circle.render();
+        fileName = "./examples/circle.svg"
+    }
+
+    fs.writeFile(fileName, renderedContent, (err)=> err ? console.log(err) : console.log("Generated logo.svg"))
+});
